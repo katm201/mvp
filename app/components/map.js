3873e72442bf06510e884ddd0f56721893c0f62b
Katherine Manning <katm201@gmail.com>
2017-11-10 14:59:06 -0800

    Implemented map component with dummy data--rendering to page

--- app/components/map.js
+++ app/components/map.js
@@ -4,7 +4,13 @@ import KEYS from '../../config'
 const UserMap = (props) => (
   <div>
     <h3>Map Here</h3>
-    {`https://maps.googleapis.com/maps/api/staticmap?center=Berkeley,CA&zoom=14&size=400x400&key=${KEYS.GOOGLE}`}
+    <iframe
+      width="600"
+      height="450"
+      frameBorder="0"
+      src={`https://www.google.com/maps/embed/v1/place?key=${KEYS.GOOGLE_MAPS}&q=${props.address}`} 
+      allowFullScreen >
+    </iframe>
   </div>
 );
 
