c5221b9bfc51407b9a3a396c842a086f3d6ec3c8
Katherine Manning <katm201@gmail.com>
2017-11-10 15:22:18 -0800

    Add thumbnail pictures to RepListItems

--- app/components/repList.js
+++ app/components/repList.js
@@ -4,8 +4,7 @@ import RepListItem from './repListItem';
 const RepList = (props) => (
   <div>
     <h3>Rep List Here</h3>
-    {console.log(props.reps.length)}
-    {props.reps.map( (rep, key) => { return <RepListItem key={key} /> } )}
+    {props.reps.map( (rep, key) => { return <RepListItem key={key} rep={rep} /> } )}
   </div>
 );
 
--- app/components/repListItem.js
+++ app/components/repListItem.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const RepListItem = (props) => (<div>RepListItem!</div>);
+const RepListItem = (props) => (
+  <div>
+    <h4>{props.rep.name}, {props.rep.party || 'Unknown'}</h4>
+    <img src={props.rep.photoUrl} className="rep-thumbnail" width="50px" />
+    <p>
+      {props.rep.phones && props.rep.phones.map( (phone, key) => { return <span key={key}>{phone}<br /></span> } )}
+      {props.rep.urls && props.rep.urls.map( (url, key) => { return <a key={key} href={url}>{url}<br /></a> } )}
+    </p>
+  </div>
+);
 
 export default RepListItem;
