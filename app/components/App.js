6d06743bf70623b758fe770dfd5e1a6b9c277a13
Katherine Manning <katm201@gmail.com>
2017-11-10 10:45:46 -0800

    Repo initialized with starter files--webpack and babel running

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.component{
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <div>App level working</div>;
+  }
+}
+
+export default App;
---
+++ app/server/app.js
@@ -0,0 +1,17 @@
+import path from 'path';
+import express from 'express';
+
+const app = express();
+
+const publicPath = express.static(path.join(__dirname, '../'));
+const indexPath = path.join(__dirname, '../index.html');
+
+app.use(publicPath);
+
+// serves our static index page
+app.get('/', (request, response) => {
+  response.sendFile(indexPath);
+});
+
+
+export default app;
