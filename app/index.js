ed389e02a176706dc5215502da8c14dacb80f4e8
Katherine Manning <katm201@gmail.com>
2017-11-11 13:45:25 -0800

    Added css loaders to webpack and did a hard reset on the browser styles

--- app/index.js
+++ app/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './public/assets/styles.css';
+
 import App from './components/App';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
