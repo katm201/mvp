a94fa77e82bfb90bf21fd1270965cf2abdf3fc2c
Katherine Manning <katm201@gmail.com>
2017-11-10 19:56:34 -0800

    Add mongoose database with schema and model

---
+++ app/server/database/db.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost/fetcher');
+
+const voterSchema = mongoose.Schema({
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    checkRequired: true
+  },
+  sessionId: {
+    type: String,
+    checkRequired: false
+  },
+  address: {
+    type: String,
+    lowercase: true,
+    checkRequired: true
+  },
+  voterInfo: {
+    type: Mixed
+  }
+});
+
+var db = mongoose.connection;
+
+const Voter = mongoose.model('Voter', voterSchema);
+
+export default Voter;
