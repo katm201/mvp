2169d1247e681c5942229d886d09445be496d08e
Katherine Manning <katm201@gmail.com>
2017-11-11 14:36:35 -0800

    Fix routing relating to default_user v. logged in users

--- app/server/middleware/databaseCheck.js
+++ app/server/middleware/databaseCheck.js
@@ -23,18 +23,19 @@ const dbUpdateAddress = (email, address, callback) => {
 
 const dbCheckEmail = (request, response, next) => {
 
-  let isPost = request.method === 'POST' && request.query.email !== 'default_user';
+  let isPost = request.method === 'POST';
+  let isUser = request.query.email !== 'default_user';
+
+  console.log('query address:', request.query.address);
 
   Voter.find({email: request.query.email}).exec()
     .then((votersInMongo) => {
-
-      // if there's a matching voter in the DB
-      if (votersInMongo.length > 0) {
+      // if there's a matching voter in the DB (that's not the default)
+      if (isUser && votersInMongo.length > 0) {
         // does the address we sent match the DB?
         let matching = request.query.address === votersInMongo[0].address;
         // if method is POST and they don't match,
         // update the DB and move on
-        console.log(request.query.address)
         if (isPost && !matching) {
           dbUpdateAddress(request.query.email, request.query.address, function() {
             next();
@@ -45,14 +46,14 @@ const dbCheckEmail = (request, response, next) => {
         if (!isPost && !matching) {
           request.query.address = votersInMongo[0].address; 
         }
-      // if there isn't a matching voter in the DB
-      } else {
-        // and if method is POST, add email and address to our DB
-        if (isPost) {
-          dbAddAll(request.query.email, request.query.address, function() {
-            next();
-          });
-        } 
+      
+      } 
+      // if it's a real user, there isn't a matching voter in the DB,
+      // and it's a POST request, then add them to the DB
+      if (isUser && isPost && votersInMongo.length === 0) {
+        dbAddAll(request.query.email, request.query.address, function() {
+          next();
+        });
       }
       next();
     })
